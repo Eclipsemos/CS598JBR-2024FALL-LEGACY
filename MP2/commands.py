27ed53abc43e391d9f90daf71eb5d22f9a57d41b
Eclms <mos7cy@163.com>
2024-10-22 01:02:54 -0500

    Commands rework & README.md more details

--- MP2/commands.py
+++ MP2/commands.py
@@ -2,43 +2,43 @@
     # This is a list of all commands you need to run for MP2 on Colab.
     ###################################################################
 
-    # TODO: Clone your GitHub repository
+    # Clone GitHub repository
     %cd /content/
     ! rm -rf ./CS598JBR-Team-9
     ! git clone https://Eclipsemos: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@github.com/Eclipsemos/CS598JBR-Team-9.git
     %cd CS598JBR-Team-9
 
-    # TODO: Replace the file path of selected_humaneval_[seed].jsonl generated in MP1
+    # Dataset generated
     input_dataset = "selected_humaneval_171768020378419351865442221048553552766.jsonl"# selected_humaneval_[seed].jsonl
 
     # Set up requirements for model prompting
     ! bash -x MP2/setup_models.sh
     %cd MP2/
-    # TODO: add your seed generated in MP1
+    # Input & Output files generated
     seed = "171768020378419351865442221048553552766"
     task_1_vanilla_json = "task_1_" + seed + "_vanilla.jsonl"
     task_1_vanilla_prompt_json = "task_1_" + seed + "_vanilla_prompt.jsonl"
-    # Generate vanilla prompt
-    ! python3 task1_script.py {input_dataset} {task_1_vanilla_prompt_json}
-    
     task_1_crafted_json = "task_1_" + seed + "_crafted.jsonl"
     task_1_crafted_prompt_json = "task_1_" + seed + "_crafted_prompt.jsonl"
+    # Vanilla prompt generate
+    ! python3 task1_script.py {input_dataset} {task_1_vanilla_prompt_json}
+    # Crafted prompt generate
     ! python3 task1_script.py {input_dataset} {task_1_crafted_prompt_json}
     
+    # [TASK1] Vanilla
+    ! python3 task_1.py {task_1_vanilla_prompt_json} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_1_vanilla_json} "True" |& tee task_1_vanilla.log
+    # [TASK1] Crafted
+    ! python3 task_1.py {task_1_crafted_prompt_json} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_1_crafted_json} "False" |& tee task_1_crafted.log
+    
+    # Input & Output files generated
     task_2_vanilla_json = "task_2_" + seed + "_vanilla.jsonl"
     task_2_crafted_json = "task_2_" + seed + "_crafted.jsonl"
     task_2_prompt_json = "task_2_" + seed + "_prompt.jsonl"
+    # Vanilla & Crafted prompt generate
     ! python3 task2_script.py {input_dataset} {task_2_prompt_json}
-
-
-    # Prompt the models, you can create your `MP2/task_1.py, MP2/task_2.py, MP2/task_3.py` by modifying `MP2/task_[ID].py`
-    # The {input_dataset} is the JSON file consisting of 20 unique programs for your group that you generated in MP1 (selected_humaneval_[seed].jsonl)
-    #! python3 task_1.py {task_1_vanilla_prompt_json} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_1_vanilla_json} "True" |& tee task_1_vanilla.log
-    #! python3 task_1.py {task_1_crafted_prompt_json} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_1_crafted_json} "False" |& tee task_1_crafted.log
-
-    #! python3 task_2.py {input_dataset} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_2_vanilla_json} "True" |& tee task_2_vanilla.log
-    #! python3 task_2.py {input_dataset} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_2_crafted_json} "False" |& tee task_2_crafted.log
-
+    ! python3 task_2.py {task_2_prompt_json} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_2_vanilla_json} "True" |& tee task_2_vanilla.log
+    ! python3 task_2.py {task_2_prompt_json} "deepseek-ai/deepseek-coder-6.7b-instruct" {task_2_crafted_json} "False" |& tee task_2_crafted.log
+`
     #%cd ..
 
     # git push all nessacery files (e.g., *jsonl, *log) to your GitHub repository
--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 
 ## Link of Google Colab Notebook
 - MP1 - [link](https://colab.research.google.com/drive/1C8Hh7bjTIQ0ETjAmL_xvoMkCaFqD19GF?usp=sharing#scrollTo=m2IyZhusX891)
-- MP2 - [link](./MP2/)
+- MP2 - [link](https://colab.research.google.com/drive/1cijisT2C9mxGjfi5xH1DekE5d9WSTjH_?usp=sharing#scrollTo=R1ux2KD4yEdk)
