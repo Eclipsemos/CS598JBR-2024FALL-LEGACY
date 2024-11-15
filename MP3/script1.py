7907838c43ae960c2ed0e6f6786c20ec3c86c688
Eclms <mos7cy@163.com>
2024-11-14 20:20:13 -0600

    run_java_code fix#7

--- MP3/script1.py
+++ MP3/script1.py
@@ -6,7 +6,7 @@ def van_generate_prompt(entry, declaration):
     prompt = f"""You are an AI programming assistant utilizing the DeepSeek Coder model, developed by DeepSeek Company, and you only answer questions related to computer science. For politically sensitive questions, security and privacy issues, and other non-computer science questions, you will refuse to answer.
 ### Instruction:
 Can you translate the following Python code into Java?
-The output Java code must be enclosed between [Java Start] and [Java End], not markdown style.
+The output Java code must be enclosed between [Java Start] and [Java End]
 The output Java code should align with declaration code and it's import module: 
 {declaration}. 
 
